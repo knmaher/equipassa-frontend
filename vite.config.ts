1498c629de0cddf3769f69ef5fd94b258f6cfed0
knmaher <maher.k.naser@gmail.com>
2025-08-05 21:17:01 +0200

    chore: fix and add test/configure files

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,20 @@
+// vite.config.ts
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    vueDevTools(),
+    vue({
+      script: {
+        defineModel: true,
+        propsDestructure: true
+      }
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
